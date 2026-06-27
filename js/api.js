7a8d40497c06332d56c5b722a5a1a6f6fa0d19da
Thiago Lemos <lemost45@gmail.com>
2026-06-27 09:43:45 -0300

    atualizando forma de verificar o acesso e fazer logout

--- js/api.js
+++ js/api.js
@@ -26,7 +26,7 @@ const api = {
     logout() {
         localStorage.removeItem('token');
         localStorage.removeItem('nome');
-        window.location.href = '../pages/login.html'; // ← corrige o caminho
+        window.location.href = '/pages/login.html';
     },
 
     // Cadastro
